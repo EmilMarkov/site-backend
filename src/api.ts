f204ea9da9557d0027b9349a4ed54643c1627884
Markov Emil <69921054+EmilMarkov@users.noreply.github.com>
2023-05-28 13:16:03 +0300

    Update api.ts

--- src/api.ts
+++ src/api.ts
@@ -16,7 +16,7 @@ app.get('/', (req, res) => {
 
 const api = express.Router();
 
-const uri = 'mongodb://mongo:SXo216P8OZjWL3ERi3Pq@containers-us-west-60.railway.app:6502';
+const uri = 'mongodb://mongo:whHEuSY1YqThFfYk1KG1@containers-us-west-166.railway.app:6668';
 
 const dbName = 'test';
 const feedbackCollectionName = 'feedback';
